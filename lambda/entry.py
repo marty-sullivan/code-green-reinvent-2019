f165b45da969c97ef425f0a769fba00673cf7258
Marty Sullivan <marty@martysullivan.com>
2019-12-05 22:50:37 +0000

    try cartopy

--- lambda/entry.py
+++ lambda/entry.py
@@ -1,13 +1,14 @@
 import matplotlib
 matplotlib.use('agg')
 
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import csv
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 from boto3.session import Session
 from datetime import datetime
-from mpl_toolkits.basemap import Basemap
 from os import environ, remove
 from PIL import Image
 from pyproj import Proj
@@ -178,42 +179,31 @@ def create_map(event):
       NextToken=resp['NextToken']
     ) if 'NextToken' in resp else None
   
-  # Initialize the Basemap used by each animation frame
+  # Initialize the Cartopy Map used by each animation frame
   img_num = 0
   fig, ax = plt.subplots()
   
-  llcrnrx, llcrnry = p(min(global_lons), min(global_lats))
-  urcrnrx, urcrnry = p(max(global_lons), max(global_lats))
+  fig = plt.figure(figsize=(10, 5))
+  ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
   
-  m = Basemap(
-    resolution='h',
-    projection='lcc',
-    rsphere=6371200.0,
-    lon_0=265.0,
-    lat_0=25.0,
-    lat_1=25.0,
-    lat_2=25.0,
-    llcrnrx=llcrnrx - 5000,
-    llcrnry=llcrnry - 5000,
-    urcrnrx=urcrnrx + 5000,
-    urcrnry=urcrnry + 5000,
-    ax=ax,
-  )
+  # ax.contourf
   
-  m.fillcontinents(color='white', lake_color='aqua')
-  m.drawcoastlines()
-  m.drawrivers(color='blue')
-  m.drawcounties(linewidth=0.5, zorder=15)
+  # llcrnrx, llcrnry = p(min(global_lons), min(global_lats))
+  # urcrnrx, urcrnry = p(max(global_lons), max(global_lats))
+  
+  ax.add_feature(cfeature.LAND.with_scale('50m'))
+  ax.add_feature(cfeature.OCEAN.with_scale('50m'))
+  ax.add_feature(cfeature.LAKES.with_scale('50m'))
+  # ax.add_feature(COUNTIES, facecolor='none', edgecolor='gray')
   
   # Draw the labels and contours for each timestep in the query results and 
   # save each as a PNG image
   for timestep, dataset in timesteps.items():
     ax.set_title('{0} {1}'.format(desc, timestep), fontdict=dict(fontfamily='monospace'))
-    x, y = m(dataset['lons'], dataset['lats'])
     
     try: 
       levels = list(np.linspace(min_val, max_val, num=25))
-      contours = plt.tricontourf(x, y, dataset['vals'], levels=levels, cmap=plt.cm.spring, alpha=0.5, antialiased=True, zorder=20)
+      contours = plt.tricontourf(dataset['lons'], dataset['lats'], dataset['vals'], levels=levels, cmap=plt.cm.spring, alpha=0.5, antialiased=True, zorder=20)
       
       if not cbar:
         cbar = fig.colorbar(contours, orientation='vertical', format='%.1f', pad=0.02)
@@ -223,8 +213,7 @@ def create_map(event):
     
     digits = []
     for i in range(len(dataset['vals'])):
-      px, py = m(dataset['lons'][i], dataset['lats'][i])
-      digit = plt.text(px, py, dataset['vals'][i], fontsize=fontsize, ha='center', va='center')
+      digit = plt.text(dataset['lons'][i], dataset['lats'][i], dataset['vals'][i], fontsize=fontsize, ha='center', va='center')
       digits.append(digit)
     
     plt.savefig('/tmp/{0:03d}.png'.format(img_num), bbox_inches='tight', dpi=300)
